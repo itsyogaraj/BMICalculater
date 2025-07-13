d187ecb43c139b35dbbb78a4f05fd3ef507082a3
Yogaraj K <Yogaraj872@gmail.com>
2025-07-13 01:58:51 -0500

    Initial commit

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function calculateBMI(){
+function calculateBMI() {
     
-    let weightInput= prompt("Enter your Weight in kg");
-    let heightInput = prompt("Enter your height in cm");
+    let weightInput = prompt("Enter your weight in kg:");
+    let heightInput = prompt("Enter your height in cm:");
 
     let weight = parseFloat(weightInput);
     let heightCm = parseFloat(heightInput);
 
     let height = heightCm / 100;
+    let bmi = weight / (height ** 2);
+   
+    let message = "";
 
-    let bmi = weight / (height**2);
+    if (bmi >= 25) {
+        message = "Not Healthy";
+    } else {
+        message = "Healthy BMI";
+    }
 
-    document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(2)}
-    `;
-
+    document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(2)} <br> ${message}`;
 }
---
+++ swicth.js
@@ -0,0 +1,16 @@
+let signal="Red";
+
+switch(signal){
+   
+    case "Red":
+    console.log("Stop");
+
+    case"Yellow":
+    console.log("Ready");
+    case"Green":
+    console.log("Go");
+
+    default:
+        console.log("Invalit Input");
+
+}
